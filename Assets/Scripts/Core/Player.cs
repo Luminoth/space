dc2552e585614bd7ed26669aaf4c00e3a1c7608b
Shane Lillie <ignignokterr@gmail.com>
2018-02-14 09:00:40 -0800

    o_O

--- Assets/Scripts/Core/Player.cs
+++ Assets/Scripts/Core/Player.cs
@@ -6,6 +6,8 @@ using UnityEngine.EventSystems;
 
 namespace EnergonSoftware.Core
 {
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Collider))]
     public sealed class Player : MonoBehavior
     {
         [SerializeField]
@@ -18,11 +20,35 @@ namespace EnergonSoftware.Core
 
         public UnityEngine.Camera Camera => _camera;
 
+        private Collider _collider;
+
 #region Unity Lifecycle
         private void Awake()
         {
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            rigidbody.isKinematic = true;
+
+            _collider = GetComponent<Collider>();
+            _collider.isTrigger = true;
+
             Camera.GetComponent<PhysicsRaycaster>().enabled = !EnableVR;
             Camera.GetComponent<GvrPointerPhysicsRaycaster>().enabled = EnableVR;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+Debug.Log($"trigger enter {other.name}");
+            other.gameObject.layer = /*Physics.IgnoreRaycastLayer*/2;
+
+            // TODO: need to disable the laser raycast hitting this object (GvrPointerPhysicsRaycaster?)
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+Debug.Log($"trigger exit {other.name}");
+            other.gameObject.layer = Physics.IgnoreRaycastLayer;
+
+            // TODO: need to enable the laser raycast hitting this object (GvrPointerPhysicsRaycaster?)
         }
 #endregion
 
